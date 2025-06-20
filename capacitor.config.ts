bba08e2cb46294164011f13024ce085b16ccd367
Yago Amorim <Swashbuckler3113@outlook.com>
2025-06-20 15:09:31 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PokeExplorer',
+  webDir: 'www'
+};
+
+export default config;
